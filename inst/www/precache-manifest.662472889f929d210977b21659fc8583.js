eb99542c09ebdee37d5e59d82d4b5fec1a694373
Scott Rigney <32641274+scottrigney@users.noreply.github.com>
2019-10-25 13:09:43 +0200

    updates for m2019 update 4

---
+++ inst/www/precache-manifest.662472889f929d210977b21659fc8583.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10aa89aad5f72f691c277bc44bfdff27",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4c2fbb08ab8720b90089",
+    "url": "./static/css/2.520b5698.chunk.css"
+  },
+  {
+    "revision": "c02a191f8f4932ec610d",
+    "url": "./static/css/main.432e7807.chunk.css"
+  },
+  {
+    "revision": "4c2fbb08ab8720b90089",
+    "url": "./static/js/2.bafc2536.chunk.js"
+  },
+  {
+    "revision": "c02a191f8f4932ec610d",
+    "url": "./static/js/main.b18e38d4.chunk.js"
+  },
+  {
+    "revision": "5eb9fe67b5985a0ab537",
+    "url": "./static/js/runtime-main.6c141fb0.js"
+  }
+]);
